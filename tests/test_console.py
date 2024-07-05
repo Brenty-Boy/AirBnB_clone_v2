5dd92d90800d77ee054bbc986c43f184f51128fd
brent hamsley <brenthamsley@gmail.com>
2024-07-05 15:05:40 -0700

    Main

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,252 +1,147 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""A unit test module for the console (command interpreter).
 """
-This test files holds all the tests needed to test the console application
-"""
+import json
+import MySQLdb
+import os
+import sqlalchemy
 import unittest
-from unittest.mock import patch
-from console import HBNBCommand
 from io import StringIO
-import re
+from unittest.mock import patch
+
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from tests import clear_stream
 
 
-class TestConsole(unittest.TestCase):
+class TestHBNBCommand(unittest.TestCase):
+    """Represents the test class for the HBNBCommand class.
     """
-    A unit test to see if the overall functinalits
-    of the console like, the precmd, the parsing, the
-    excution, unkown command handling, and so on are working
-    """
-    pass
-
-
-class TestCreate(unittest.TestCase):
-    """
-    A unit test class to thest all the create functionalities
-    of the create console command.
-    """
-
-    def test_create_noArgs(self):
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
+    def test_fs_create(self):
+        """Tests the create command with the file storage.
         """
-        Tests the create method the one with out no arguments
+        with patch('sys.stdout', new=StringIO()) as cout:
+            cons = HBNBCommand()
+            cons.onecmd('create City name="Texas"')
+            mdl_id = cout.getvalue().strip()
+            clear_stream(cout)
+            self.assertIn('City.{}'.format(mdl_id), storage.all().keys())
+            cons.onecmd('show City {}'.format(mdl_id))
+            self.assertIn("'name': 'Texas'", cout.getvalue().strip())
+            clear_stream(cout)
+            cons.onecmd('create User name="James" age=17 height=5.9')
+            mdl_id = cout.getvalue().strip()
+            self.assertIn('User.{}'.format(mdl_id), storage.all().keys())
+            clear_stream(cout)
+            cons.onecmd('show User {}'.format(mdl_id))
+            self.assertIn("'name': 'James'", cout.getvalue().strip())
+            self.assertIn("'age': 17", cout.getvalue().strip())
+            self.assertIn("'height': 5.9", cout.getvalue().strip())
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
+    def test_db_create(self):
+        """Tests the create command with the database storage.
         """
+        with patch('sys.stdout', new=StringIO()) as cout:
+            cons = HBNBCommand()
+            # creating a model with non-null attribute(s)
+            with self.assertRaises(sqlalchemy.exc.OperationalError):
+                cons.onecmd('create User')
+            # creating a User instance
+            clear_stream(cout)
+            cons.onecmd('create User email="john25@gmail.com" password="123"')
+            mdl_id = cout.getvalue().strip()
+            dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER'),
+                passwd=os.getenv('HBNB_MYSQL_PWD'),
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+            cursor = dbc.cursor()
+            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(mdl_id))
+            result = cursor.fetchone()
+            self.assertTrue(result is not None)
+            self.assertIn('john25@gmail.com', result)
+            self.assertIn('123', result)
+            cursor.close()
+            dbc.close()
 
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create")
-        clean_output = out_put.getvalue()
-        self.assertEqual(clean_output, "** class name missing **\n")
-
-    def test_create_unExistingClass(self):
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
+    def test_db_show(self):
+        """Tests the show command with the database storage.
         """
-        Tests the creat method when the class to be created doesn't
-        exist.
+        with patch('sys.stdout', new=StringIO()) as cout:
+            cons = HBNBCommand()
+            # showing a User instance
+            obj = User(email="john25@gmail.com", password="123")
+            dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER'),
+                passwd=os.getenv('HBNB_MYSQL_PWD'),
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+            cursor = dbc.cursor()
+            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(obj.id))
+            result = cursor.fetchone()
+            self.assertTrue(result is None)
+            cons.onecmd('show User {}'.format(obj.id))
+            self.assertEqual(
+                cout.getvalue().strip(),
+                '** no instance found **'
+            )
+            obj.save()
+            dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER'),
+                passwd=os.getenv('HBNB_MYSQL_PWD'),
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+            cursor = dbc.cursor()
+            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(obj.id))
+            clear_stream(cout)
+            cons.onecmd('show User {}'.format(obj.id))
+            result = cursor.fetchone()
+            self.assertTrue(result is not None)
+            self.assertIn('john25@gmail.com', result)
+            self.assertIn('123', result)
+            self.assertIn('john25@gmail.com', cout.getvalue())
+            self.assertIn('123', cout.getvalue())
+            cursor.close()
+            dbc.close()
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
+    def test_db_count(self):
+        """Tests the count command with the database storage.
         """
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create NOCLASS")
-        clean_output = out_put.getvalue()
-        self.assertEqual(clean_output, "** class doesn't exist **\n")
-
-    def test_create_withclassName(self):
-        """
-        Tests the create method with a correcet class name
-        """
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create User")
-        clean_output = out_put.getvalue()
-        # check if the printed value is something like an id
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("User."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-        # do some more test with other class names
-
-    def test_create_classWithOneParams(self):
-        """
-        Test the create method with paramaetrs
-        """
-
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=\"California\"")
-        clean_output = out_put.getvalue()
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"name\": \"California\"", db)
-            self.assertTrue(len(result) == 1)
-        # do some more test with other class names
-
-    def test_create_classWithMoreParams(self):
-        """
-        Test the create method with paramaetrs
-        """
-
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=\"California\
-                    " city_id=\"0001\" user_id=\"0001\"")
-
-        clean_output = out_put.getvalue()
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"name\": \"California\"", db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"city_id\": \"0001\"", db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"user_id\": \"0001\"", db)
-            self.assertTrue(len(result) == 1)
-        # do some more test with other class names
-
-    def test_create_valueTypesStrWithDoubleQuote(self):
-        """
-        Test the create method with paramaetrs that have a string value
-        """
-
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=\"Double_quote_Insid\"e\"")
-        clean_output = out_put.getvalue()
-
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall(r'"name": "Double quote Insid\\"e"', db)
-            self.assertTrue(len(result) == 1)
-        # do some more test with other class names
-
-    def test_create_valueTypeStrWithSpace(self):
-        """
-        Test the create method with paramaetrs that have a string value
-        """
-
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=\"my_little_house\"")
-        clean_output = out_put.getvalue()
-
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"name\": \"my little house\"", db)
-            self.assertTrue(len(result) == 1)
-        # do some more test with other class names
-
-    def test_create_valueTypeFloat(self):
-        """
-        Test the create method with paramaetrs that have a string value
-        """
-
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=15.6")
-        clean_output = out_put.getvalue()
-
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"name\": 15.6", db)
-            self.assertTrue(len(result) == 1)
-
-    def test_create_valueTypeInt(self):
-        """
-        Test the create method with paramaetrs that have a string value
-        """
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=15")
-        clean_output = out_put.getvalue()
-
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"name\": 15", db)
-            self.assertTrue(len(result) == 1)
-
-    def test_create_valueTypeWrong(self):
-        """
-        Test the create method with paramaetrs that have a string value
-        """
-        # delete the database to avoid conflicting
-        # names
-        from models import storage as f_storage
-        f_storage.empty()
-
-        with patch('sys.stdout', new=StringIO()) as out_put:
-            HBNBCommand().onecmd("create Place name=_15")
-        clean_output = out_put.getvalue()
-
-        self.assertRegex(clean_output, r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\n")
-        # see if this created object is stored in the file.json
-        with open("file.json", "r") as storage:
-            db = storage.read()
-            # check if the data is stored correclt in the file storage
-            result = re.findall("Place."+clean_output[:-1], db)
-            self.assertTrue(len(result) == 1)
-
-            result = re.findall("\"name\": \"_15\"", db)
-            self.assertTrue(len(result) == 0)
-
-
-class TestShow(unittest.TestCase):
-    """Test show"""
-    pass
+        with patch('sys.stdout', new=StringIO()) as cout:
+            cons = HBNBCommand()
+            dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER'),
+                passwd=os.getenv('HBNB_MYSQL_PWD'),
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+            cursor = dbc.cursor()
+            cursor.execute('SELECT COUNT(*) FROM states;')
+            res = cursor.fetchone()
+            prev_count = int(res[0])
+            cons.onecmd('create State name="Enugu"')
+            clear_stream(cout)
+            cons.onecmd('count State')
+            cnt = cout.getvalue().strip()
+            self.assertEqual(int(cnt), prev_count + 1)
+            clear_stream(cout)
+            cons.onecmd('count State')
+            cursor.close()
+            dbc.close()
