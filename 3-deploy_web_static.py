b0a0331206a2a1689959d39b2dd6444b1840398c
brent hamsley <brenthamsley@gmail.com>
2024-07-09 17:48:34 -0700

    Main

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,57 +14,61 @@ env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_pack():
-"""
-Create a compressed archive of web_static contents.
-Returns path of the created archive, or None if failed.
-"""
+    """
+    Create a compressed archive of web_static contents.
+    Returns path of the created archive, or None if failed.
+    """
+
+
 try:
-now = datetime.now().strftime('%Y%m%d%H%M%S')
-archive_path = 'versions/web_static_{}.tgz'.format(now)
-local('mkdir -p versions')
-local('tar -cvzf {} web_static'.format(archive_path))
-print('web_static packed: {} -> {}Bytes'.format(archive_path, os.path.getsize(archive_path)))
-        
-return archive_path
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_{}.tgz'.format(now)
+    local('mkdir -p versions')
+    local('tar -cvzf {} web_static'.format(archive_path))
+    print('web_static packed: {} -> {}Bytes'.format(
+        archive_path, os.path.getsize(archive_path)))
+
+    return archive_path
 except Exception as e:
     print(e)
 return None
 
 
 def do_deploy(archive_path):
-"""
-Distribute an archive to the web servers and deploy it.
-"""
+    """
+    Distribute an archive to the web servers and deploy it.
+    """
+
+
 if not archive_path or not os.path.exists(archive_path):
-return False
+    return False
 
 try:
     # Upload archive to /tmp/ on the remote server
-put(archive_path, '/tmp/')
+    put(archive_path, '/tmp/')
 
-        # Extract archive to /data/web_static/releases/<archive_name>/
-        archive_filename = os.path.basename(archive_path).split('.')[0]
-        release_path = '/data/web_static/releases/{}/'.format(archive_filename)
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(
-            archive_filename, release_path))
+    # Extract archive to /data/web_static/releases/<archive_name>/
+    archive_filename = os.path.basename(archive_path).split('.')[0]
+    release_path = '/data/web_static/releases/{}/'.format(archive_filename)
+    run('mkdir -p {}'.format(release_path))
+    run('tar -xzf /tmp/{}.tgz -C {}'.format(archive_filename, release_path))
 
-        # Remove uploaded archive from /tmp/
-        run('rm /tmp/{}.tgz'.format(archive_filename))
+    # Remove uploaded archive from /tmp/
+    run('rm /tmp/{}.tgz'.format(archive_filename))
 
-        # Move contents to the proper location
-        run('mv {}web_static/* {}'.format(release_path, release_path))
+    # Move contents to the proper location
+    run('mv {}web_static/* {}'.format(release_path, release_path))
 
-        # Delete the now-empty web_static directory
-        run('rm -rf {}web_static'.format(release_path))
+    # Delete the now-empty web_static directory
+    run('rm -rf {}web_static'.format(release_path))
 
-        # Create symbolic link
-        current_path = '/data/web_static/current'
-        run('rm -rf {}'.format(current_path))
-        run('ln -s {} {}'.format(release_path, current_path))
+    # Create symbolic link
+    current_path = '/data/web_static/current'
+    run('rm -rf {}'.format(current_path))
+    run('ln -s {} {}'.format(release_path, current_path))
 
-        print('New version deployed!')
-        return True
+    print('New version deployed!')
+    return True
     except Exception as e:
         print(e)
         return False
