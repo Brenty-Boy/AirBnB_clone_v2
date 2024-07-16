8ba304d994dcb4afe0c9dfca4145b8c69c9cae74
Brenty-Boy <125645093+Brenty-Boy@users.noreply.github.com>
2024-07-17 01:37:07 +0200

    Main

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,11 @@ This module instantiates an object of class DBStorage
 if the environment variable HBNB_TYPE_STORAGE is equal to db,
 otherwise it instantiates an object of class FileStorage
 """
+
 from os import getenv
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
